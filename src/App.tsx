44198f4a0ffc3b4151e0881df7c2d512a82c7778
Van Ni <vanni4018@gmail.com>
2024-05-24 21:40:19 +0700

    refactor: remove unnessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-// import { Board } from "./pages/Boards/_id"
-import { Board } from "@pages/Boards/_id"
-// #Trello3 : Absolute path not working in Vite project React TS
+
 function App() {
   return (
     <>
-    <Board/>
+      hello world
     </>
   )
 }
